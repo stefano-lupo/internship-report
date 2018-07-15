00f2dfd5d8bcbfd8ef5c330b023f2c8f93b09092
Stefano <lupos@tcd.ie>
2018-07-15 15:10:25 +0100

    Added Immutability

---
+++ code/technologies/immutability/LoginAttemptLogger.java
@@ -0,0 +1,13 @@
+class LoginAttemptLogger {
+  LoginRequestDao loginRequestDao;
+
+  public LoginAttemptLogger(LoginRequestDao loginRequestDao) {
+    this.loginRequestDao = loginRequestDao;
+  }
+
+  public void logLoginAttempt(LoginRequest loginRequest) {
+    // Don't log the users password to the database
+    loginRequest.setPassword("");
+    loginRequestDao.writeToTable(loginRequest);
+  }
+}
